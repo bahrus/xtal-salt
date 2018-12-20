eaa38249af37f52c0f70dbd4abe56881f96bd3ce
bahrus <anderson.bruce.b@gmail.com>
2018-12-20 10:23:06 -0500

    ...

--- dist/xtal-salt.iife.js
+++ dist/xtal-salt.iife.js
@@ -111,6 +111,7 @@ class XtalSalt extends XtallatX(HTMLElement) {
     constructor() {
         super(...arguments);
         this._domParser = new DOMParser();
+        this._target = null;
         this._c = false;
     }
     static get is() { return 'xtal-salt'; }
@@ -120,6 +121,21 @@ class XtalSalt extends XtallatX(HTMLElement) {
     set xmlString(nv) {
         this._xmlString = nv;
         this._xml = this._domParser.parseFromString(nv, 'application/xml');
+        this.onPropsChange();
+    }
+    get xml() {
+        return this._xml;
+    }
+    set xml(nv) {
+        const ua = navigator.userAgent;
+        if ((ua.indexOf('Firefox') > -1) || (ua.indexOf('Edge') > -1)) {
+            const outerHTML = nv.outerHTML;
+            if (outerHTML) {
+                this.xmlString = outerHTML;
+                return;
+            }
+        }
+        this._xml = nv;
         this.onPropsChange();
     }
     get xslString() {
@@ -141,14 +157,21 @@ class XtalSalt extends XtallatX(HTMLElement) {
     }
     connectedCallback() {
         this._c = true;
-        this._upgradeProperties([disabled, 'xmlString', 'xslString']);
+        this.style.display = 'none';
+        this._upgradeProperties([disabled, 'xmlString', 'xslString', 'xml']);
         this.onPropsChange();
     }
     onPropsChange() {
         if (this._disabled || !this._c || !this._xml || !this._xsltProcessor)
             return;
-        if (!this._target) {
-            this._target = this;
+        if (this._target === null) {
+            this._target = this.nextElementSibling;
+        }
+        if (this._target === null) {
+            setTimeout(() => {
+                this.onPropsChange();
+            }, 50);
+            return;
         }
         this.style.display = (this._target === this) ? 'block' : 'none';
         this._target.innerHTML = '';
